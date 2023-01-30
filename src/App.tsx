278ab57e2e68cc9139997b8bb8ce6ac72951cf02
MariaShev777 <mariashevtsova1403@gmail.com>
2023-01-30 23:43:20 +0300

    grids

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img alt="nike"
+                     src="https://1000logos.net/wp-content/uploads/2021/11/Nike-Logo.png"
+                />
+            </header>
+            <nav className="nav">
+                <div>Profile</div>
+                <div>Messages</div>
+            </nav>
+            <div className="content">
+                Main content
+            </div>
+        </div>
+    );
 }
 
 export default App;
